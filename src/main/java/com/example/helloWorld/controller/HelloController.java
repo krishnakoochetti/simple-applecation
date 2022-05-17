e6ced41e5ec58688f48663fca82e6267a560100a
krishnaraj <krishnaraj3010@gmail.com>
2022-05-17 11:58:44 +0530

    simple commit

---
+++ src/main/java/com/example/helloWorld/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.helloWorld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String Helloworld(){
+        return "hello";
+    }
+
+    @RequestMapping("/hello")
+        public String devops(){
+        return "devops";
+    }
+}
