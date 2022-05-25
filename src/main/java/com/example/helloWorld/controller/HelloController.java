5ecda307773950b2af1c7bf849404df4ac92eba7
Krishna Koochetti <70144166+krishnakoochetti@users.noreply.github.com>
2022-05-25 10:19:37 +0530

    Update HelloController.java

--- src/main/java/com/example/helloWorld/controller/HelloController.java
+++ src/main/java/com/example/helloWorld/controller/HelloController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/jenkins-app")
+    @RequestMapping("/")
     public String Helloworld(){
         return "hello";
     }
